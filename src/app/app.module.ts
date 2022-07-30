0759443d922c588bf7daf7bbb10754873b6380fc
David Villegas <davidvillegasayala@gmail.com>
2022-07-30 16:54:08 -0500

    Hotfix: brands component was not full deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { WelcomeComponent } from './feature/welcome/welcome.component';
 import { OffersComponent } from './feature/offers/offers.component';
-import { BrandsComponent } from './feature/brands/brands.component';
 import { CampaignsComponent } from './feature/campaigns/campaigns.component';
 import { DealershipComponent } from './feature/dealership/dealership.component';
 import { ShoppingToolsComponent } from './feature/shopping-tools/shopping-tools.component';
@@ -18,7 +17,6 @@ import { ShoppingToolsComponent } from './feature/shopping-tools/shopping-tools.
     FooterComponent,
     WelcomeComponent,
     OffersComponent,
-    BrandsComponent,
     CampaignsComponent,
     DealershipComponent,
     ShoppingToolsComponent,
